22cb7e1cc793152131b35140b5ecb0f83e5b0807
Chase Johnson <charles-e-johnson@uiowa.edu>
2019-04-16 00:13:00 -0500

    Able to write a user with an id and first name to the user table in the database.

--- main.py
+++ main.py
@@ -8,10 +8,11 @@ from flask import Flask
 import uuid
 
 from werkzeug.utils import secure_filename
-
+import datetime
 app = Flask(__name__)
 
 import base64
+
 
 
 def dbconn():
@@ -26,6 +27,10 @@ def home():
 @app.route('/register')
 def register():
     return render_template('register.html')
+
+@app.route('/adduser')
+def addUser():
+    return render_template('AddUser.html')
 
 
 # @app.route('/result', methods=['POST', 'GET'])
@@ -49,6 +54,34 @@ def viewall():
     conn.close()
     return render_template("list.html", rows=data)
 
+@app.route('/adduser', methods=['POST', 'GET'])
+def users():
+    if request.method == 'POST':
+        # try:
+        nm = request.form['FlightName']
+
+        conn = dbconn()
+
+        sql = "INSERT INTO users(idusers, first_name) VALUES(%s, %s)"
+        cursor = conn.cursor()
+
+        id = get_uuid()
+        print("UUID is " + str(id))
+        cursor.execute(sql, (id, nm))
+        # cursor.execute("INSERT INTO users(idusers, first_name) VALUES("+id+",'"+nm+"')")
+        # cursor.execute("INSERT INTO users(idusers, first_name) VALUES(7,'"+nm+"')")
+        # cursor.execute("INSERT INTO users(idusers, first_name) VALUES(8,'test')")
+
+
+        conn.commit()
+        msg = "Record successfully added"
+        # except Exception as e:
+        #     print("ERR: ",e)
+        #     msg = "Error in Insert operation"
+        #     conn.rollback()
+        # finally:
+        return render_template("users.html", result=request.form, msg=msg)
+        conn.close()
 
 @app.route('/addrec', methods=['POST', 'GET'])
 def addrec():
@@ -58,12 +91,12 @@ def addrec():
 
         conn = dbconn()
 
-        sql = "INSERT INTO flights(idflights, Departing_City) VALUES(%d, %s)"
+        # sql = "INSERT INTO users(idflights, Departing_City) VALUES(%d, %s)"
         cursor = conn.cursor()
         # id = uuid.uuid4()
         # print("UUID is " + id)
         # cursor.execute(sql, (2, nm))
-        cursor.execute("INSERT INTO flights(idflights, Departing_City) VALUES(4,'test')")
+        cursor.execute("INSERT INTO users(idusers, first_name) VALUES(4,'test')")
         conn.commit()
         msg = "Record successfully added"
         # except Exception as e:
@@ -74,6 +107,10 @@ def addrec():
         return render_template("addrec.html", result=request.form, msg=msg)
         conn.close()
 
+def get_uuid():
+    id = str(datetime.datetime.now().year) + str(datetime.datetime.now().month) + str(datetime.datetime.now().day) + str(
+    datetime.datetime.now().hour) + str(datetime.datetime.now().second)
+    return id
 
 if __name__ == '__main__':
     app.run(debug=True)
